de8c4f49525ab4aa13ab2b0efc50534a1e0c8602
joshu <joshuapetitma@yahoo.com>
2021-04-03 22:30:10 -0400

    Add example implementation of model

--- src/ooga/model/Model.java
+++ src/ooga/model/Model.java
@@ -1,7 +1,52 @@
 package ooga.model;
 
+import com.google.common.base.Preconditions;
+import com.google.common.collect.Multimap;
+import java.util.function.Consumer;
 import ooga.model.observables.ObservableModel;
+import ooga.model.observables.ObservableObject;
 
-public interface Model extends ObservableModel {
-  void checkCollisions();
+public class Model implements ObservableModel {
+  Multimap<String, Runnable> functionMap;
+  public Model() {
+    String name = Preconditions.checkNotNull("osjfa");
+  }
+
+  public void handleCode(String k) {
+    var v = functionMap.get(k);
+    for(Runnable func : v) {
+      func.run();
+    }
+  }
+
+  public void step() {
+
+  }
+
+  public void advanceLevel() {
+
+  }
+
+  @Override
+  public void setOnLevelChange(Consumer<String> callback) {
+
+  }
+
+  @Override
+  public void setOnNewObject(Consumer<ObservableObject> callback) {
+
+  }
+
+  @Override
+  public void setOnTextUpdate(Consumer<String> text) {
+
+  }
+
+  public void play() {
+
+  }
+
+  private void checkCollisions() {
+
+  }
 }
