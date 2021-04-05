dce7cbffbbe5f1607348fefc3f9cc1edce4b4a13
Jiyang Tang <jt304@duke.edu>
2021-04-05 13:14:52 +0800

    Use reflections to create component in ComponentManager

--- src/ooga/model/systems/ComponentManager.java
+++ src/ooga/model/systems/ComponentManager.java
@@ -1,5 +1,6 @@
 package ooga.model.systems;
 
+import java.lang.reflect.InvocationTargetException;
 import ooga.model.GameObject;
 import ooga.model.components.Component;
 
@@ -19,7 +20,23 @@ public class ComponentManager extends BaseSystem {
   public void update() {
   }
 
-  public <T extends Component> T createComponent(GameObject owner, Class<T> componentClass) {
-    return null;
+  public <T extends Component> T createComponent(
+      GameObject owner, Class<T> componentClass
+  ) {
+    // TODO: log errors
+    T ret = null;
+    try {
+      var constructor = componentClass.getConstructor(int.class, GameObject.class);
+      ret = constructor.newInstance(idManager.getNewId(), owner);
+    } catch (NoSuchMethodException e) {
+      System.out.println(
+          "Cannot find a valid constructor in component class: " + componentClass.getName()
+      );
+    } catch (IllegalAccessException | InvocationTargetException | InstantiationException e) {
+      System.out.println(
+          "Failed to instantiate component class: " + componentClass.getName()
+      );
+    }
+    return ret;
   }
 }
