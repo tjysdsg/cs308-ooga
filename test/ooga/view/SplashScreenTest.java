a3e0a574999dc3c96e5209b29c2a4d142c03f7d7
joshu <joshuapetitma@yahoo.com>
2021-04-07 14:50:48 -0400

    Finish tests for splash screen

--- test/ooga/view/SplashScreenTest.java
+++ test/ooga/view/SplashScreenTest.java
@@ -1,11 +1,11 @@
 package ooga.view;
 
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicIntegerFieldUpdater;
 import java.util.function.Predicate;
 import javafx.scene.Node;
+import javafx.scene.control.Button;
 import javafx.scene.control.ButtonBase;
-import org.hamcrest.Matcher;
+import javafx.stage.Stage;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.testfx.api.FxAssert;
@@ -13,52 +13,44 @@ import org.testfx.api.FxRobot;
 import org.testfx.framework.junit5.ApplicationExtension;
 import org.testfx.framework.junit5.Start;
 import org.testfx.matcher.control.LabeledMatchers;
-
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
 import org.testfx.util.WaitForAsyncUtils;
 
 @ExtendWith(ApplicationExtension.class)
 class TestViewManaging {
 
-    private View view;
-    private Stage stage;
-    private static final Predicate<Node> verifyFade = node -> node.getOpacity() < 1;
-    private static final Predicate<Node> isPresent = Node::isVisible;
+  private View view;
+  private Stage stage;
+  private static final Predicate<Node> verifyFade = node -> node.getOpacity() < 1;
+  private static final Predicate<Node> isPresent = Node::isVisible;
 
-    @Start
-    private void start(Stage stage) {
-        this.stage = stage;
-        this.view = new View(stage);
-    }
+  @Start
+  private void start(Stage stage) {
+    this.stage = stage;
+    this.view = new View(stage);
+  }
 
-    /**
-     * @param robot - Will be injected by the test runner.
-     */
-    @Test
-    void splashScreenButtonsVisible(FxRobot robot) {
-        FxAssert.verifyThat("#load-game-splash", (Predicate<Button>) ButtonBase::isVisible);
-        FxAssert.verifyThat("#load-game-splash", LabeledMatchers.hasText("Load Game"));
-        FxAssert.verifyThat("#settings-splash", (Predicate<Button>) ButtonBase::isVisible);
-        FxAssert.verifyThat("#settings-splash", LabeledMatchers.hasText("Settings"));
-    }
+  @Test
+  void splashScreenButtonsVisible(FxRobot robot) {
+    FxAssert.verifyThat("#load-game-splash", (Predicate<Button>) ButtonBase::isVisible);
+    FxAssert.verifyThat("#load-game-splash", LabeledMatchers.hasText("Load Game"));
+    FxAssert.verifyThat("#settings-splash", (Predicate<Button>) ButtonBase::isVisible);
+    FxAssert.verifyThat("#settings-splash", LabeledMatchers.hasText("Settings"));
+  }
 
-    @Test
-    void gameSelectAppears(FxRobot robot) {
-        robot.clickOn("#load-game-splash");
-        WaitForAsyncUtils.sleep(300, TimeUnit.MILLISECONDS);
-        FxAssert.verifyThat("#splash-screen", verifyFade);
-        WaitForAsyncUtils.sleep(1, TimeUnit.SECONDS);
-        FxAssert.verifyThat("#selection-scene", isPresent);
-        FxAssert.verifyThat("#add-game-button", isPresent);
-    }
+  @Test
+  void gameSelectAppears(FxRobot robot) {
+    robot.clickOn("#load-game-splash");
+    WaitForAsyncUtils.sleep(300, TimeUnit.MILLISECONDS);
+    FxAssert.verifyThat("#splash-screen", verifyFade);
+    WaitForAsyncUtils.sleep(1, TimeUnit.SECONDS);
+    FxAssert.verifyThat("#selection-scene", isPresent);
+    FxAssert.verifyThat("#add-game-button", isPresent);
+  }
 
-    @Test
-    void exitApplicationWorks(FxRobot robot) {
-        robot.clickOn("#exit-splash");
-        WaitForAsyncUtils.sleep(500, TimeUnit.MILLISECONDS);
-        FxAssert.verifyThat("#splash-screen", verifyFade);
-    }
+  @Test
+  void exitApplicationWorks(FxRobot robot) {
+    robot.clickOn("#exit-splash");
+    WaitForAsyncUtils.sleep(500, TimeUnit.MILLISECONDS);
+    FxAssert.verifyThat("#splash-screen", verifyFade);
+  }
 }
