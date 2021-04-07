d036f7bb5d332959ba5cb306502348ccee8d2ea7
joshu <joshuapetitma@yahoo.com>
2021-04-07 13:13:40 -0400

    Add handling for actions within systems

--- src/ooga/model/systems/ActionManager.java
+++ src/ooga/model/systems/ActionManager.java
@@ -1,2 +1,37 @@
-package ooga.model.systems;public class ActionManager {
+package ooga.model.systems;
+
+import com.google.common.collect.ListMultimap;
+import com.google.common.collect.MultimapBuilder;
+import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+import ooga.model.actions.ActionInfo;
+import ooga.model.objects.GameObject;
+import ooga.model.util.CollisionAction;
+import org.checkerframework.checker.units.qual.C;
+
+public class ActionManager {
+  ListMultimap<String, Consumer<CollisionAction>> actions;
+
+  public ActionManager() {
+    actions = MultimapBuilder.hashKeys().arrayListValues().build();
+  }
+  public void registerAction(String actionName, Consumer<CollisionAction> action) {
+    actions.put(actionName, action);
+  }
+
+  public void handleAction(GameObject self, GameObject other, ActionInfo a) {
+    List<ActionInfo> actionInfos = self.getActions();
+    for(var ai : actionInfos) {
+      if (ai.equals(a)) {
+        doAction(ai, self, other);
+      }
+    }
+  }
+
+  private void doAction(ActionInfo action, GameObject self, GameObject other) {
+    List<Consumer<CollisionAction>> doActions = actions.get(action.getAction());
+    doActions.forEach(e -> e.accept(new CollisionAction(self, other, action.getPayload())));
+  }
+
 }
--- src/ooga/model/util/CollisionAction.java
+++ src/ooga/model/util/CollisionAction.java
@@ -2,14 +2,26 @@ package ooga.model.util;
 
 import ooga.model.objects.GameObject;
 
-public class ActionEvent {
+public class CollisionAction {
 
   private GameObject self;
   private GameObject hitter;
+  private String payload;
 
-  public ActionEvent(GameObject self, GameObject hitter) {
-
+  public CollisionAction(GameObject self, GameObject hitter, String payload) {
     this.self = self;
     this.hitter = hitter;
+  }
+
+  public GameObject getSelf() {
+    return self;
+  }
+
+  public GameObject getHitter() {
+    return hitter;
+  }
+
+  public String getPayload() {
+    return payload;
   }
 }
