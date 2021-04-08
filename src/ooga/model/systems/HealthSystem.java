0d039948e045259e0ccefb460f0c1fd6f5b2933c
Tinglong Zhu <tz88@duke.edu>
2021-04-08 13:24:25 +0800

    adding health systemn

---
+++ src/ooga/model/systems/HealthSystem.java
@@ -0,0 +1,12 @@
+package ooga.model.systems;
+
+import ooga.model.System;
+
+public class HealthSystem extends ComponentBasedSystem {
+
+  public HealthSystem(ComponentManager componentManager, Class componentType) {
+    super(componentManager, componentType);
+  }
+
+
+}
