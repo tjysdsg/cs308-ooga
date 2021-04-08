bacc4f8cc24e906a2d5aab161a6da303e4c63789
Jiyang Tang <jt304@duke.edu>
2021-04-08 16:00:29 +0800

    Implement ActionInfo methods

--- src/ooga/model/actions/ActionInfo.java
+++ src/ooga/model/actions/ActionInfo.java
@@ -1,16 +1,30 @@
 package ooga.model.actions;
 
-import com.google.common.base.MoreObjects;
+import java.util.Objects;
 
 public class ActionInfo {
+
   private String with = "Enemy";
   private String position = "Left";
   private String action = "jump_self";
   private String payload = "50";
 
+  public ActionInfo(String with, String position, String action, String payload) {
+    this.with = with;
+    this.position = position;
+    this.action = action;
+    this.payload = payload;
+  }
+
   @Override
-  public boolean equals(Object obj) {
-    return false;
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof ActionInfo that)) {
+      return false;
+    }
+    return Objects.equals(with, that.with) && Objects.equals(position, that.position);
   }
 
   public String getAction() {
