8cd9864e0cb38dc4cef9482e8c284218b860d7d6
Tinglong Zhu <tz88@duke.edu>
2021-04-08 13:49:44 +0800

    health system structure finished

--- src/ooga/model/systems/HealthSystem.java
+++ src/ooga/model/systems/HealthSystem.java
@@ -1,11 +1,38 @@
 package ooga.model.systems;
 
+import java.util.List;
 import ooga.model.System;
+import ooga.model.objects.GameObject;
 
-public class HealthSystem extends ComponentBasedSystem {
+/**
+ * Managing the Health System and the destroy detection.
+ */
+public class HealthSystem extends GameObjectBasedSystem {
+  protected List<GameObject> gameObjects;
+  protected EntityManager entityManager;
 
-  public HealthSystem(ComponentManager componentManager, Class componentType) {
-    super(componentManager, componentType);
+
+  public HealthSystem(EntityManager entityManager) {
+    super(entityManager);
+    this.entityManager=entityManager;
+  }
+
+  //TODO: How to access the Game Object Health
+  public void healthChange(int ID, int health){
+    gameObjects.setHealth(gameObjects.getHealt()-health);
+  }
+
+  protected void singleObjectDestroy(GameObject gameObject){
+    //TODO:set the boundary health for each game
+    if(gameObject.getHealth()<=0){
+      //TODO: Where to put the break of the gameobjects?
+    }
+  }
+
+  public void destroyDetection(){
+    for(GameObject gameObject: getTrackedGameObjects()){
+      singleObjectDestroy(gameObject);
+    }
   }
 
 
