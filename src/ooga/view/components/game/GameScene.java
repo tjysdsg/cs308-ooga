e7dccae19b20192c9aa580561630e73c516fbd68
joshu <joshuapetitma@yahoo.com>
2021-04-11 16:04:30 -0400

    Move game scene and object view to game package

--- src/ooga/view/components/game/GameScene.java
+++ src/ooga/view/components/game/GameScene.java
@@ -1,19 +1,22 @@
-package ooga.view.components;
+package ooga.view.components.game;
 
 import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 import ooga.view.ModelController;
 import ooga.model.observables.ObservableModel;
 import ooga.view.util.ObservableResource;
 
 /** A scene in which games are actually tracked and played. */
 public class GameScene extends Scene {
-  private ModelController controller;
+  private StackPane root;
   private ObservableModel game;
+  private ModelController controller;
+  private String directory;
 
-  public GameScene(ObservableModel game, ObservableResource resources) {
-    super(new Pane());
-    this.game = game;
+  public GameScene(String directory, ObservableResource resources) {
+    super(new StackPane());
+    this.root = (StackPane) getRoot();
+    this.directory = directory;
     //this.controller = game.getController();
   }
 
