f3c497cf52a36884df59039382127011edad409c
Robert Barnette <rhb19@duke.edu>
2021-04-12 15:26:44 -0400

    added collision detection code to system class

--- src/ooga/model/systems/CollisionSystem.java
+++ src/ooga/model/systems/CollisionSystem.java
@@ -1,5 +1,10 @@
 package ooga.model.systems;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import ooga.model.objects.GameObject;
+
 public class CollisionSystem extends GameObjectBasedSystem{
 
   private ActionManager myActionManager;
@@ -14,4 +19,47 @@ public class CollisionSystem extends GameObjectBasedSystem{
   public void update(double deltaTime) {
 
   }
+
+  private void checkCollisions() {
+    List<GameObject> objects = getTrackedGameObjects();
+    List<GameObject> collidableObjects = new ArrayList<>();
+
+    for(GameObject gameObject: objects){
+      if(gameObject.isA("collidable")){
+        collidableObjects.add(gameObject);
+      }
+    }
+    Collections.sort(collidableObjects);
+    findCollisions(collidableObjects);
+  }
+
+  private void findCollisions(List<GameObject> collidableObjects) {
+    for(int k =0; k<collidableObjects.size()-1; k++){
+      executeCollisions(k, collidableObjects);
+    }
+  }
+
+  private void executeCollisions(int index, List<GameObject> collidableObjects) {
+    GameObject collidingObject = collidableObjects.get(index);
+    double width = collidingObject.getWidth();
+    double height = collidingObject.getHeight();
+    double x = collidingObject.getX();
+    double y = collidingObject.getY();
+
+    for(int k = index+1; k<collidableObjects.size(); k++){
+      GameObject collidedObject = collidableObjects.get(k);
+
+      if(x + width < collidedObject.getX()){
+        return;
+      }
+      else if(collidedObject.getY() >= y && collidedObject.getY() <= y+height){
+        collide(collidingObject, collidedObject);
+      }
+
+    }
+  }
+
+  public void collide(GameObject collidingObject, GameObject collidedObject) {
+  }
+
 }
