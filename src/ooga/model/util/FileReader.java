5e95dcd5e1a3eeec30e3a876ab72b1436cb18199
Oliver Rodas <oar5@duke.edu>
2021-04-14 00:05:55 -0400

    Added a file reader with static methods to read files

---
+++ src/ooga/model/util/FileReader.java
@@ -0,0 +1,34 @@
+package ooga.model.util;
+
+import ooga.model.exceptions.NotADirectoryException;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class FileReader {
+    public static String readFile(File toConvert) throws FileNotFoundException {
+        Path filePath = toConvert.toPath();
+
+        try {
+            return Files.readString(filePath);
+        } catch (IOException e) {
+            throw new FileNotFoundException(toConvert.getName());
+        }
+    }
+
+    public static File getFile(File directory, String fileName) throws FileNotFoundException {
+        if (!directory.isDirectory()) {
+            throw new NotADirectoryException(directory.getName());
+        }
+        File[] levelMatches = directory.listFiles((file, s) -> s.equals(fileName));
+
+        if (levelMatches == null) {
+            throw new FileNotFoundException();
+        }
+
+        return levelMatches[0];
+    }
+}
