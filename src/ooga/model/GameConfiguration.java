35e0f87ed8d2bada8453ffc36a42bbb5618a12e0
Oliver Rodas <oar5@duke.edu>
2021-04-14 00:07:35 -0400

    Added a game configuration file to hold moshi config items

---
+++ src/ooga/model/GameConfiguration.java
@@ -0,0 +1,16 @@
+package ooga.model;
+
+import ooga.model.exceptions.InvalidDataFileException;
+
+public class GameConfiguration {
+    public static final String CONFIG_JSON = "config.json";
+    private String startLevel;
+
+    public String getStartLevel() {
+        if (startLevel == null) {
+            throw new InvalidDataFileException(CONFIG_JSON);
+        }
+        return startLevel;
+    }
+}
+
