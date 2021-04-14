38c938992f3994997731dc7e6660b95ae25b321b
Jiyang Tang <jt304@duke.edu>
2021-04-14 10:20:56 +0800

    Implement player running

--- src/ooga/model/components/PlayerComponent.java
+++ src/ooga/model/components/PlayerComponent.java
@@ -1,12 +1,89 @@
 package ooga.model.components;
 
-import com.google.common.collect.Multimap;
 import ooga.model.objects.GameObject;
 
 public class PlayerComponent extends Component {
+
+  public static final int RIGHT_DIRECTION = 1;
+  public static final int LEFT_DIRECTION = -1;
+  private static final int DEFAULT_MAX_SPEED = 100;
+
+  public enum HorizontalMovementStatus {
+    /**
+     * Not moving
+     */
+    STILL,
+    /**
+     * Running at maxSpeed
+     */
+    RUNNING,
+  }
+
+  public enum VerticalMovementStatus {
+    /**
+     * Standing/running on ground
+     */
+    GROUNDED,
+    /**
+     * Falling in air
+     */
+    FALLING,
+    /**
+     * Jumping, different from FALLING because the player position is rising
+     */
+    RISING,
+  }
+
+  /**
+   * which horizontal the player is facing, 1 for right, -1 for left
+   */
+  private int direction = RIGHT_DIRECTION;
+
+
+  /**
+   * Status of the player in terms of vertical movement
+   */
+  private VerticalMovementStatus verticalStatus = VerticalMovementStatus.GROUNDED;
+
+  /**
+   * Status of the player in terms of horizontal movement
+   */
+  private HorizontalMovementStatus horizontalStatus = HorizontalMovementStatus.STILL;
+
+  /**
+   * Max horizontal movement speed
+   */
+  private double maxSpeed = DEFAULT_MAX_SPEED;
 
   public PlayerComponent(int id, GameObject owner) {
     super(id, owner);
   }
 
+  public int getDirection() {
+    return direction;
+  }
+
+  public void setDirection(int direction) {
+    this.direction = direction;
+  }
+
+  public double getMaxSpeed() {
+    return maxSpeed;
+  }
+
+  public void setMaxSpeed(double maxSpeed) {
+    this.maxSpeed = maxSpeed;
+  }
+
+  public void switchDirection() {
+    direction = -direction;
+  }
+
+  public void setHorizontalStatus(HorizontalMovementStatus horizontalStatus) {
+    this.horizontalStatus = horizontalStatus;
+  }
+
+  public HorizontalMovementStatus getHorizontalStatus() {
+    return horizontalStatus;
+  }
 }
--- src/ooga/model/systems/PlayerSystem.java
+++ src/ooga/model/systems/PlayerSystem.java
@@ -1,33 +1,74 @@
 package ooga.model.systems;
 
+import java.util.List;
 import ooga.model.annotations.Track;
 import ooga.model.components.PlayerComponent;
+import ooga.model.components.PlayerComponent.HorizontalMovementStatus;
 import ooga.model.objects.GameObject;
 
+// TODO: add tests for this
 @Track(PlayerComponent.class)
 public class PlayerSystem extends ComponentBasedSystem {
 
+  private ComponentMapper<PlayerComponent> componentMapper;
+  // TODO: support active and inactive players
+
   public PlayerSystem(ECManager ecManager) {
     super(ecManager);
+    componentMapper = getComponentMapper(PlayerComponent.class);
   }
 
   public void init() {
     addMapping("right", this::handleRight);
+    addMapping("left", this::handleLeft);
     addMapping("jump", this::handleJump);
     addCollisionMapping("jump_self", event -> handleJump(event.getSelf()));
+  }
+
+  public List<PlayerComponent> getPlayers() {
+    return componentMapper.getComponents();
   }
 
   public void handleJump(GameObject obj) {
   }
 
-  void handleRight(boolean on) {
+  private void handleHorizontalMovement(boolean run, int direction) {
+    List<PlayerComponent> players = getPlayers();
+    for (PlayerComponent p : players) {
+      p.setDirection(direction);
+      if (run) {
+        p.setHorizontalStatus(HorizontalMovementStatus.RUNNING);
+      } else {
+        p.setHorizontalStatus(HorizontalMovementStatus.STILL);
+      }
+    }
   }
 
+  private void handleRight(boolean on) {
+    handleHorizontalMovement(on, PlayerComponent.RIGHT_DIRECTION);
+  }
+
+  private void handleLeft(boolean on) {
+    handleHorizontalMovement(on, PlayerComponent.LEFT_DIRECTION);
+  }
 
   void handleJump(boolean on) {
+    List<PlayerComponent> players = getPlayers();
+    for (PlayerComponent p : players) {
+      // TODO: implement jumping mechanism
+    }
   }
 
   @Override
   public void update(double deltaTime) {
+    List<PlayerComponent> players = getPlayers();
+    for (PlayerComponent p : players) {
+      GameObject go = p.getOwner();
+      go.setVelocityX(
+          p.getHorizontalStatus() == HorizontalMovementStatus.RUNNING
+              ? p.getDirection() * p.getMaxSpeed() : 0
+      );
+      // TODO: set vertical velocity accordingly go.setVelocityY();
+    }
   }
 }
