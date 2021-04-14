3bc645c4a0c5a4d950d690e216cde0640bd50d0a
Tinglong Zhu <tz88@duke.edu>
2021-04-14 13:57:49 +0800

    NPC movement horizontal finished

--- src/ooga/model/systems/NPCSystem.java
+++ src/ooga/model/systems/NPCSystem.java
@@ -1,17 +1,22 @@
 package ooga.model.systems;
 
+import ooga.model.components.MovementSquenceComponent;
 import ooga.model.components.PlayerComponent;
 
-import java.util.List;
-
 public class NPCSystem extends PlayerSystem{
+    protected ComponentMapper<MovementSquenceComponent> movementSequenceMapper;
 
     public NPCSystem(ECManager ecManager) {
         super(ecManager);
+        movementSequenceMapper =getComponentMapper(MovementSquenceComponent.class);
     }
 
     @Override
     public void init(){
         initPlayerType(PlayerComponent.PlayerType.NEUTRAL);
     }
+
+    private void moveRight()
+
+
 }
