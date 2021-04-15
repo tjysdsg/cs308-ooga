54169e2f7b6e6a3e8a15afd55deae7e8c2c7df64
Tinglong Zhu <tz88@duke.edu>
2021-04-15 21:00:25 +0800

    attack update logic finished

---
+++ src/ooga/model/systems/creature/SampleAttackSystem.java
@@ -0,0 +1,11 @@
+package ooga.model.systems.creature;
+
+import ooga.model.systems.ECManager;
+
+import java.util.Map;
+
+public class SampleAttackSystem extends AttackSystems{
+    public SampleAttackSystem(ECManager ecManager) {
+        super(ecManager);
+    }
+}
