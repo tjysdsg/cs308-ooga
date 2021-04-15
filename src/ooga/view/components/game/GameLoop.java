3adc9e054c4230e6f9f0d7688479fe5b79272d87
joshu <joshuapetitma@yahoo.com>
2021-04-15 11:11:41 -0400

    Create gameloop class

--- src/ooga/view/components/game/GameLoop.java
+++ src/ooga/view/components/game/GameLoop.java
@@ -1,20 +1,26 @@
 package ooga.view.components.game;
 
+import java.util.function.Consumer;
+
 import javafx.animation.AnimationTimer;
 
 public class GameLoop extends AnimationTimer {
-  private Runnable callback;
+  private Consumer<Double> callback;
+  private long prevTime;
+  private double delta;
 
   @Override
   public void handle(long currentNanoTime) {
+    delta = currentNanoTime - prevTime;
+    delta /= 1e9;
     if (callback != null) {
-      callback.run();
+      callback.accept(delta);
     }
   }
 
   public void setUpdateTime(long timeInNano) {}
 
-  public void setOnUpdate(Runnable run) {
+  public void setOnUpdate(Consumer<Double> run) {
     this.callback = run;
   }
 }
