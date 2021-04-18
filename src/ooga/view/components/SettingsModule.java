7d1ae0ad528fa1b1e2620417b397d1351aab1ee7
joshu <joshuapetitma@yahoo.com>
2021-04-18 12:16:19 -0400

    Create settings pane class and setting modules

---
+++ src/ooga/view/components/SettingsModule.java
@@ -0,0 +1,53 @@
+package ooga.view.components;
+
+import com.jfoenix.controls.JFXComboBox;
+
+import javafx.beans.binding.StringBinding;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.StringProperty;
+import javafx.collections.ObservableList;
+import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import ooga.view.util.ObservableResource;
+
+public class SettingsModule extends StackPane {
+  private FlowPane area;
+
+  public SettingsModule(ObservableResource resources) {
+    getStyleClass().addAll("settings-module");
+    area = new FlowPane();
+    area.getStyleClass().addAll("settings-area");
+    getChildren().addAll(area);
+  }
+
+  /**
+   * Add a setting to the settings pane and return a binding
+   * to track its updates.
+   *
+   * @param label - The label lol
+   */
+  public StringBinding addFieldSettings(StringBinding text) {
+    Label label = new Label();
+    label.textProperty().bind(text);
+    return null;
+  }
+
+  public ObjectProperty<String> addListSetting(StringBinding text, ObservableList<String> list) {
+    JFXComboBox<String> comboBox = new JFXComboBox<>(list);
+    Label label = new Label();
+    label.textProperty().bind(text);
+    createPair(label, comboBox);
+    return comboBox.valueProperty();
+  }
+
+  private void createPair(Label label, Node value) {
+    HBox box = new HBox();
+    box.getStyleClass().addAll("settings-pair");
+    box.getChildren().addAll(label, value);
+    area.getChildren().addAll(box);
+  }
+
+}
---
+++ src/ooga/view/components/SettingsPane.java
@@ -0,0 +1,17 @@
+package ooga.view.components;
+
+import javafx.beans.binding.StringBinding;
+import javafx.collections.ObservableList;
+import javafx.scene.layout.StackPane;
+import ooga.view.util.ObservableResource;
+
+public class SettingsPane extends StackPane {
+
+  public SettingsPane(ObservableResource resources) {
+
+  }
+
+  public void addModule(SettingsModule module) {
+    // TODO: Create Tabbed pane to add modules
+  }
+}
