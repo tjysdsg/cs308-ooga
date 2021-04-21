1ed0d883ad8e2d3b2e10d58b9608ebcce592cfb5
Oliver Rodas <oliver.rodas@duke.edu>
2021-04-21 00:42:16 -0400

    Added actions class to have multiple payloads

---
+++ src/ooga/model/actions/Action.java
@@ -0,0 +1,11 @@
+package ooga.model.actions;
+
+import java.util.List;
+
+public class Action {
+  private List<String> payload;
+
+  public List<String> getPayloads() {
+    return payload;
+  }
+}
