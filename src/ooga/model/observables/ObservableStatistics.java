0d426b2cf98b4053f7a660365b7d2615986396b0
Tinglong Zhu <tz88@duke.edu>
2021-04-22 05:45:43 +0800

    start working on statistics

--- src/ooga/model/observables/ObservableStatistics.java
+++ src/ooga/model/observables/ObservableStatistics.java
@@ -3,7 +3,7 @@ package ooga.model.observables;
 import java.util.List;
 import java.util.function.Consumer;
 
-interface ObservableStatistics {
+public interface ObservableStatistics {
     public void setOnStatisticUpdate(String statistic, Consumer<String> consumer);
     public List<String> getTrackableStatistics();
 }
