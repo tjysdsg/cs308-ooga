c9edc802db604b6cac0d124e54e8d0081159828a
Jiyang Tang <jt304@duke.edu>
2021-04-22 16:29:50 +0800

    Add stats tracking in HealthSystem

--- src/ooga/model/systems/HealthSystem.java
+++ src/ooga/model/systems/HealthSystem.java
@@ -1,5 +1,8 @@
 package ooga.model.systems;
 
+import java.util.ArrayList;
+import java.util.List;
+import ooga.model.StatsInfo;
 import ooga.model.annotations.Track;
 import ooga.model.components.HealthComponent;
 
@@ -9,11 +12,22 @@ import ooga.model.components.HealthComponent;
 @Track(HealthComponent.class)
 public class HealthSystem extends ComponentBasedSystem {
 
+  private static final String HEALTH_STATS_NAME = "health";
+
   ComponentMapper<HealthComponent> componentMapper;
 
   public HealthSystem(ECManager ecManager) {
     super(ecManager);
     componentMapper = getComponentMapper(HealthComponent.class);
+    addStatsSupplier(HEALTH_STATS_NAME, this::healthStatsSupplier);
+  }
+
+  private List<StatsInfo> healthStatsSupplier() {
+    ArrayList<StatsInfo> ret = new ArrayList<>();
+    for (HealthComponent healthComponent : componentMapper.getComponents()) {
+      ret.add(new StatsInfo(healthComponent.getHealth() + "", healthComponent.getOwner().getId()));
+    }
+    return ret;
   }
 
   /**
@@ -24,10 +38,14 @@ public class HealthSystem extends ComponentBasedSystem {
    */
   public void changeHealth(int entityId, double delta, boolean increase) {
     componentMapper.get(entityId).healthIncrement(delta, increase);
+
+    triggerStatsUpdate(HEALTH_STATS_NAME);
   }
 
   protected void destroyObject(int entityId) {
     getECManager().deleteGameObject(entityId);
+
+    triggerStatsUpdate(HEALTH_STATS_NAME);
   }
 
   public void destroyDetection() {
