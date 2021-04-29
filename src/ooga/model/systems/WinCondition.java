2d5ace62fe44f0b089fdd7e703b31391f926469e
Robert Barnette <rhb19@duke.edu>
2021-04-29 15:14:04 -0400

    added javadocs

--- src/ooga/model/systems/WinCondition.java
+++ src/ooga/model/systems/WinCondition.java
@@ -1,5 +1,9 @@
 package ooga.model.systems;
 
+/**
+ * @author Robert Barnette This class is used to represent a particular win or loss condition for a
+ * WinComponent
+ */
 public class WinCondition {
 
   private String condition;
@@ -7,33 +11,54 @@ public class WinCondition {
   private boolean win;
   private String operator;
 
-  public WinCondition(){
+  /**
+   * This constructor is used by MOSHI to create WinCondition objects
+   */
+  public WinCondition() {
 
   }
 
-  public WinCondition(String condition, double threshold, String operator, boolean win){
+  /**
+   * @param condition is the type of win condition being created (score, health, enemy)
+   * @param threshold is the threshold value defining the condition
+   * @param operator  is the operator being used to compare the current value and the threshold (==,
+   *                  <, >)
+   * @param win       is the boolean specifying if this is a win or loss condition
+   */
+  public WinCondition(String condition, double threshold, String operator, boolean win) {
     this.condition = condition;
     this.threshold = threshold;
     this.operator = operator;
     this.win = win;
   }
 
-  public boolean checkCondition(double input){
-    if(operator.equals("equal")) return input == threshold;
-    else if(operator.equals("lt")) return input < threshold;
-    else if(operator.equals("gt")) return input > threshold;
+  /**
+   * @param input is the current value of the condition
+   * @returns a boolean determining if the win or loss condition has been satisfied
+   */
+  public boolean checkCondition(double input) {
+    if (operator.equals("equal")) {
+      return input == threshold;
+    } else if (operator.equals("lt")) {
+      return input < threshold;
+    } else if (operator.equals("gt")) {
+      return input > threshold;
+    }
 
     return false;
   }
 
+  /**
+   * @returns the current condition
+   */
   public String getCondition() {
     return condition;
   }
 
-  public double getThreshold() {
-    return threshold;
-  }
 
+  /**
+   * @returns a boolean describing if this is a win condition
+   */
   public boolean isWin() {
     return win;
   }
--- src/ooga/model/systems/WinSystem.java
+++ src/ooga/model/systems/WinSystem.java
@@ -10,9 +10,14 @@ import ooga.model.components.WinComponent;
 import ooga.model.components.enemy.HateComponent;
 import ooga.model.managers.ECManager;
 
+/**
+ * @author Robert Barnette This class checks the win or loss status of a particular level and ends
+ * the level when a win or loss condition is met
+ */
 @Track({PlayerComponent.class, HealthComponent.class, ScoreComponent.class, WinComponent.class,
     HateComponent.class})
-public class WinSystem extends ComponentBasedSystem{
+public class WinSystem extends ComponentBasedSystem {
+
   private ComponentMapper<PlayerComponent> playerMapper;
   private ComponentMapper<HealthComponent> healthMapper;
   private ComponentMapper<ScoreComponent> scoreMapper;
@@ -22,9 +27,13 @@ public class WinSystem extends ComponentBasedSystem{
 
   private boolean hasWon = false;
 
+  /**
+   * @param ecManager is the ecManager used to create the WinSystem This constructor creates a
+   *                  WinSystem object from an ECManager
+   */
   public WinSystem(ECManager ecManager) {
     super(ecManager);
-    playerMapper=getComponentMapper(PlayerComponent.class);
+    playerMapper = getComponentMapper(PlayerComponent.class);
     healthMapper = getComponentMapper(HealthComponent.class);
     scoreMapper = getComponentMapper(ScoreComponent.class);
     winMapper = getComponentMapper(WinComponent.class);
@@ -41,39 +50,45 @@ public class WinSystem extends ComponentBasedSystem{
     );
   }
 
+  /**
+   * @param deltaTime is the time elapsed from the last game loop This method is called every cycle
+   *                  of the game loop and checks the level to see if a win or loss condition has
+   *                  been met
+   */
   @Override
   public void update(double deltaTime) {
     List<WinComponent> winComps = winMapper.getComponents();
 
-    if(winComps.size() == 0){
+    if (winComps.size() == 0) {
       return;
     }
 
-    for(WinComponent w: winComps){
+    for (WinComponent w : winComps) {
       checkWin(w);
     }
 
   }
 
+  /**
+   * @param w is the WinComponent whose win conditions are being checked This method checks all win
+   *          conditions present in a win component and will end the level if a condition is
+   *          triggered
+   */
   private void checkWin(WinComponent w) {
     List<WinCondition> winConditions = w.getWinConds();
-    for(WinCondition wCond: winConditions){
+    for (WinCondition wCond : winConditions) {
 
-      if(wCond.getCondition().equals("score")){
+      if (wCond.getCondition().equals("score")) {
         double score = scoreMapper.get(w.getOwner().getId()).getScore();
         boolean comp = wCond.checkCondition(score);
         executeWinOrLose(comp, wCond);
         return;
-      }
-
-      else if(wCond.getCondition().equals("health")){
+      } else if (wCond.getCondition().equals("health")) {
         double health = healthMapper.get(w.getOwner().getId()).getHealth();
         boolean comp = wCond.checkCondition(health);
         executeWinOrLose(comp, wCond);
         return;
-      }
-
-      else if(wCond.getCondition().equals("enemy")) {
+      } else if (wCond.getCondition().equals("enemy")) {
         boolean comp = wCond.checkCondition(hateMapper.getComponents().size());
         executeWinOrLose(comp, wCond);
         return;
@@ -81,33 +96,49 @@ public class WinSystem extends ComponentBasedSystem{
     }
 
   }
-  public void setSetOnLevelEnd(Consumer<Boolean> setOnLevelEnd){
+
+  /**
+   * @param setOnLevelEnd is the callback that is called when a win or lose condition is triggered
+   *                      This method allows the user to pass a callback that will be executed when
+   *                      a win or loss condition is triggered
+   */
+  public void setSetOnLevelEnd(Consumer<Boolean> setOnLevelEnd) {
     this.setOnLevelEnd = setOnLevelEnd;
   }
 
+  /**
+   * @param comp  is the boolean returned by the checkcondition method of wCond
+   * @param wCond is the win condition that has been checked If comp is true then a win or loss
+   *              condition is triggered based on the type of condition wCond is
+   */
   private void executeWinOrLose(boolean comp, WinCondition wCond) {
-    if(comp && wCond.isWin()){
+    if (comp && wCond.isWin()) {
       winGame();
     }
-    if(comp && !wCond.isWin()){
+    if (comp && !wCond.isWin()) {
       loseGame();
     }
   }
 
-  private void loseGame(){
-    if(!hasWon){
+  /**
+   * Ends the level when a lose condition is triggered
+   */
+  private void loseGame() {
+    if (!hasWon) {
       hasWon = true;
       setOnLevelEnd.accept(false);
     }
   }
 
-  private void winGame(){
-    if(!hasWon){
-      hasWon = true; 
+  /**
+   * Ends the level when a win condition is triggered
+   */
+  private void winGame() {
+    if (!hasWon) {
+      hasWon = true;
       setOnLevelEnd.accept(true);
     }
   }
-
 
 
 }
