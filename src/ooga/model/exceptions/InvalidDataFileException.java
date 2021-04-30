a8a327d1839ddb729f9ce999eba7764ced894a2a
Oliver Rodas <oliver.rodas@duke.edu>
2021-04-30 12:47:30 -0400

    InvalidDataFileException javadoc

--- src/ooga/model/exceptions/InvalidDataFileException.java
+++ src/ooga/model/exceptions/InvalidDataFileException.java
@@ -1,10 +1,18 @@
 package ooga.model.exceptions;
 
+/**
+ * Used to inform the user that a file was not properly formatted
+ * @author Oliver Rodas
+ */
 public class InvalidDataFileException extends ModelException {
 
   private static final String format =
       "Error reading %s. Please check to make sure the file is formatted as a proper json file.%n";
 
+  /**
+   * Create a new exception
+   * @param fileName The filename
+   */
   public InvalidDataFileException(String fileName) {
     super(String.format(format, fileName));
   }
