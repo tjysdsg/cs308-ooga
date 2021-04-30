413a6ea731ec45bc6876e063110958d94a6fe830
Oliver Rodas <oliver.rodas@duke.edu>
2021-04-30 12:06:34 -0400

    ActionInfo JavaDoc

--- src/ooga/model/actions/ActionInfo.java
+++ src/ooga/model/actions/ActionInfo.java
@@ -3,6 +3,11 @@ package ooga.model.actions;
 import java.util.List;
 import java.util.Set;
 
+
+/**
+ * Action Info is the main object used in collisions. It encompasses actions to create an object
+ * that collisions can use.
+ */
 public class ActionInfo {
 
   private static final String ANY_ITEM = "any";
@@ -11,6 +16,9 @@ public class ActionInfo {
   private Set<String> positions = Set.of(ANY_SIDE);
   private List<Action> actions;
 
+  /**
+   * Create an action info with the default parameters
+   */
   public ActionInfo() {
   }
 
@@ -31,6 +39,10 @@ public class ActionInfo {
     return ret && (positions.contains(ANY_SIDE) || positions.contains(info.position()));
   }
 
+  /**
+   * Get the actions associated with this collision
+   * @return the list of actions to be done if this collision is triggered
+   */
   public List<Action> getActions() {
     return this.actions;
   }
