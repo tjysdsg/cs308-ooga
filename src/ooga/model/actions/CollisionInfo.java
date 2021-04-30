dd1f88ff283a8166ef9da54617759d3c22c3dcaa
Oliver Rodas <oliver.rodas@duke.edu>
2021-04-30 12:17:59 -0400

    CollisionInfo JavaDoc

--- src/ooga/model/actions/CollisionInfo.java
+++ src/ooga/model/actions/CollisionInfo.java
@@ -2,6 +2,9 @@ package ooga.model.actions;
 
 import ooga.model.objects.GameObject;
 
+/**
+ * Meant to be used by a collision system to describe a collision apart of its action
+ */
 public record CollisionInfo(GameObject self, GameObject other, String position) {
 
 }
