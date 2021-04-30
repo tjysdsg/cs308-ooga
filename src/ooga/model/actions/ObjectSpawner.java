c973a645e81a25939801dcc5cbd8f6e37433e93f
Oliver Rodas <oliver.rodas@duke.edu>
2021-04-30 12:37:33 -0400

    ObjectSpawner JavaDoc

--- src/ooga/model/actions/ObjectSpawner.java
+++ src/ooga/model/actions/ObjectSpawner.java
@@ -1,21 +1,35 @@
 package ooga.model.actions;
 
-import java.util.Timer;
 import ooga.model.managers.ECManager;
 import ooga.model.objects.GameObject;
 import ooga.model.objects.ObjectInstance;
 
+/**
+ * Object Spawner is meant to be used to have any action create arbitrary objects.
+ *
+ * It is meant to be instantiated using the the ecManager to be able to create the objects
+ */
 public class ObjectSpawner {
   private String payload;
   private ECManager ecManager;
   private double cooldown;
   private long start = System.nanoTime();
 
+  /**
+   * Create a new object spawner
+   * @param payload The payload of the spawner to use
+   * @param ecManager the manager to use to create objects
+   */
   public ObjectSpawner(String payload, ECManager ecManager) {
     this.payload = payload;
     this.ecManager = ecManager;
   }
 
+  /**
+   * Spawn an object using the id of the parent
+   * @param id The id of the parent object
+   * @param on if to do the spawn
+   */
   public void handleSpawn(Integer id, boolean on) {
     if (on) {
       double elapsedTime = (Long.valueOf(System.nanoTime() - start)).doubleValue()/1000000000;
@@ -27,6 +41,7 @@ public class ObjectSpawner {
       }
     }
   }
+
 
   private ObjectInstance parsePayload(String payload, GameObject original) {
     String[] params = payload.split(",");
