12562856a3c3ac4604461b7e4799242937429dba
Oliver Rodas <oliver.rodas@duke.edu>
2021-04-30 13:21:27 -0400

    JavaDocs

--- src/ooga/model/exceptions/ModelException.java
+++ src/ooga/model/exceptions/ModelException.java
@@ -1,8 +1,18 @@
 package ooga.model.exceptions;
 
+/**
+ * An abstract exception class that can be caught by the view  or whatever is running the game
+ *
+ * It is meant to be extended with whatever is needed to be made
+ * @author Oliver Rodas
+ */
 public abstract class ModelException extends RuntimeException {
 
-  ModelException(String message) {
+  /**
+   * Create a new model exception
+   * @param message the message to return
+   */
+  public ModelException(String message) {
     super(message);
   }
 }
--- src/ooga/model/exceptions/NotADirectoryException.java
+++ src/ooga/model/exceptions/NotADirectoryException.java
@@ -1,7 +1,17 @@
 package ooga.model.exceptions;
 
+/**
+ *
+ * This exception catches an error if a directory was not passed to something that needs one
+ *
+ * @author Oliver Rodas
+ */
 public class NotADirectoryException extends ModelException {
 
+  /**
+   * Creates a new exception
+   * @param dirName the file attempted to be used
+   */
   public NotADirectoryException(String dirName) {
     super(String.format("Error: %s must be a directory.", dirName));
   }
